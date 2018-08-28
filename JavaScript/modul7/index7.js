6c4bb72814ea5bd3b33a7badacbd997dfc2d8190
TonyCh55 <chuprunant@gmail.com>
2018-08-28 11:07:18 +0300

    'fdsg'

---
+++ JavaScript/modul7/index7.js
@@ -0,0 +1,34 @@
+`use strict`;
+const posts = [
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/nature",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
+    }
+];
+
+const container = document.querySelector('.container');
+
+const createPostCard = ({img,title,text,link})=>
+    `<div class="post">
+    <img class="post__image" src="${img}" alt="post image">
+    <h2 class="post__title"${title}</h2>
+    <p class="post__text">${text}</p>
+    <a class="button" href="#">${link}</a>
+</div>`;
+
+const createCards = arr => arr.reduce((acc, card) => acc + createPostCard(card),``);
+container.insertAdjacentHTML('afterbegin',createCards(posts));
